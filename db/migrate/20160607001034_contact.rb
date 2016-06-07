513c6c0493d8bcf7ee7a6fe75e166b6c2f68193c
Yejiel <yejielw@hotmail.com>
2016-06-07 00:22:34 +0000

    added contacts route

--- db/migrate/20160607001034_contact.rb
+++ db/migrate/20160607001034_contact.rb
@@ -1,4 +1,10 @@
 class Contact < ActiveRecord::Migration
   def change
+    create_table :contacs do |t|
+      t.string :name
+      t.string :email
+      t.text :comments
+      
+      t.timestamps
   end
 end
